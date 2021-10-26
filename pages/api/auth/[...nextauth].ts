95398a09d1ce1ab79180a5f93ce55ba1b3107866
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2021-10-26 18:02:40 +0200

    feat: replace github provider with email

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,37 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import emailParser from "email-addresses";
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import EmailAuthProvider from "next-auth/providers/email";
 
+const prisma = new PrismaClient();
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    signIn({ user }) {
+      if (typeof user.email !== "string") return false;
+      const parsed = emailParser.parseOneAddress(user.email);
+      const isAllowedToSignIn =
+        parsed &&
+        "domain" in parsed &&
+        (parsed.domain === "drieets.gouv.fr" ||
+          parsed.domain === "beta.gouv.fr");
+      if (isAllowedToSignIn) return true;
+      return false;
+      // Or you can return a URL to redirect to: return '/unauthorized'
+    },
+  },
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    EmailAuthProvider({
+      from: process.env.EMAIL_FROM,
+      server: {
+        auth: {
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+          user: process.env.EMAIL_SERVER_USER,
+        },
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+      },
     }),
   ],
 });
