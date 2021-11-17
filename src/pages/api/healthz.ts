dbbdb5a63554afdca0433bfecb05b1300c601481
Adrien Di Pasquale <adrien@dipasquale.fr>
2021-11-17 15:51:29 +0100

    feat: /api/healthz probe
    closes #63

---
+++ src/pages/api/healthz.ts
@@ -0,0 +1,12 @@
+import type { NextApiHandler } from "next";
+
+const handler: NextApiHandler = (req, res) => {
+  if (req.method !== "GET") {
+    res.status(405).end();
+    return;
+  }
+
+  res.status(200).json({ all: "good" });
+};
+
+export default handler;
