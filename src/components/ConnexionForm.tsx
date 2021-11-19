f74a0582cef14850178c154c9ab2004b2ad435aa
Adrien Di Pasquale <adrien@dipasquale.fr>
2021-11-19 10:03:31 +0100

    refactor: minor rename in ConnexionForm

--- src/components/ConnexionForm.tsx
+++ src/components/ConnexionForm.tsx
@@ -6,7 +6,7 @@ interface Props {
   signinRequired: boolean;
 }
 
-const Layout: React.FC<Props> = ({ csrfToken, signinRequired }) => {
+const ConnexionForm: React.FC<Props> = ({ csrfToken, signinRequired }) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.formWrapper}>
@@ -48,4 +48,4 @@ const Layout: React.FC<Props> = ({ csrfToken, signinRequired }) => {
   );
 };
 
-export default Layout;
+export default ConnexionForm;
