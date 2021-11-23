27ef8e8855e396654ffc54ae28127528a73f20c5
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2021-11-23 18:28:56 +0100

    WIP

--- src/pages/api/dossiers/[id].ts
+++ src/pages/api/dossiers/[id].ts
@@ -1,5 +1,5 @@
 import type { StatutDossier } from "@prisma/client";
-import type { NextApiHandler } from "next";
+import type { NextApiHandler, NextApiRequest } from "next";
 import { getSession } from "next-auth/react";
 import getPrismaClient from "src/lib/prismaClient";
 import type { TransitionEvent } from "src/lib/statutDossierStateMachine";
@@ -7,24 +7,52 @@ import { factory as statutDossierStateMachineFactory } from "src/lib/statutDossi
 import superjson from "superjson";
 
 const handler: NextApiHandler = async (req, res) => {
-  const prisma = getPrismaClient();
-  const { id: dossierIdStr } = req.query;
-  if (typeof dossierIdStr !== "string") {
-    res.status(404).send(`${dossierIdStr} is not a valid dossier id`);
-    return;
-  }
-  const dossierId = parseInt(dossierIdStr, 10);
-
-  if (req.method !== "PUT") {
-    res.status(405).end();
-    return;
-  }
-
   const session = await getSession({ req });
   if (!session) {
     res.status(401).end();
     return;
   }
+  const { id: dossierIdStr } = req.query;
+  if (typeof dossierIdStr !== "string") {
+    res.status(404).send(`not a valid dossier id`);
+    return;
+  }
+
+  if (req.method == "PUT") {
+    await update(req, res);
+  } else if (req.method == "GET") {
+    await get(req, res);
+  } else {
+    res.status(405).end();
+    return;
+  }
+};
+
+function getDossierId(req: NextApiRequest): number {
+  const { id: dossierIdStr } = req.query;
+  return parseInt(dossierIdStr as string, 10);
+}
+
+const get: NextApiHandler = async (req, res) => {
+  const prisma = getPrismaClient();
+  const dossierId = getDossierId(req);
+
+  const dossier = await prisma.dossier.findUnique({
+    include: {
+      commission: true,
+      demandeur: true,
+      enfants: true,
+      societeProduction: true,
+      user: true,
+    },
+    where: { id: dossierId },
+  });
+
+  res.status(200).json(superjson.stringify(dossier));
+};
+
+const update: NextApiHandler = async (req, res) => {
+  const prisma = getPrismaClient();
 
   if (typeof req.body !== "string") {
     res.status(400).end();
@@ -38,6 +66,7 @@ const handler: NextApiHandler = async (req, res) => {
   }
 
   const updates: { statut?: StatutDossier; userId?: number } = {};
+  const dossierId = getDossierId(req);
 
   if (typeof parsed.transitionEvent === "string") {
     const transition = parsed.transitionEvent;
