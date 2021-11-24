6fe2059682539a1d8b9b22c29948af3861031653
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2021-11-24 18:54:17 +0100

    fix: re-fix prisma connection limit in local with global (#90)

--- src/lib/prismaClient.ts
+++ src/lib/prismaClient.ts
@@ -2,6 +2,21 @@
 
 import { PrismaClient } from "@prisma/client";
 
-const client = new PrismaClient();
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | null;
+  // function getPrismaClient(): PrismaClient;
+}
+
+const getClient = () => {
+  if (process.env.NODE_ENV === "production") {
+    return new PrismaClient();
+  } else {
+    if (!global.prisma) global.prisma = new PrismaClient();
+    return global.prisma;
+  }
+};
+
+const client = getClient();
 
 export default client;
