ff72617d4e6f2adf707813f832aa1af8687af6eb
Madyo <y.lebars@numericite.eu>
2022-05-03 15:32:29 +0200

    fix: test delete migration

--- prisma/migrations/20220502081421_add_fields_for_upload/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterEnum
-ALTER TYPE "Role" ADD VALUE 'MEMBRE';
-
--- AlterTable
-ALTER TABLE "Commission" ADD COLUMN     "lastSent" TIMESTAMP(3);
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "departement" TEXT;
