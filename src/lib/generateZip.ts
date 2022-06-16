c582a35e1c20a0a4766d1763d968290b107a96d5
YoannNumericite <85347776+YoannNumericite@users.noreply.github.com>
2022-06-16 17:32:26 +0200

    fix: fix errors zip (#336)

--- src/lib/generateZip.ts
+++ src/lib/generateZip.ts
@@ -22,7 +22,9 @@ export default (commission: CommissionData) => {
   _.forEach(
     commission.dossiers,
     (dossier: DossierData & { files: unknown[] }) => {
-      const dossierFolder = zip.folder(dossier.nom.replace(".", "_"));
+      const dossierFolder = zip.folder(
+        dossier.nom.replace(".", "_").replace("'", "_")
+      );
 
       const roles = _.uniq(
         dossier.enfants.map((e: Enfant) => {
