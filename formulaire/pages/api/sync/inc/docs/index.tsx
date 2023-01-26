c782227c9dd47c6963e02aa966c06277241a2f77
YoannNumericite <85347776+YoannNumericite@users.noreply.github.com>
2023-01-26 19:27:22 +0100

    feat: view docs from instructeurs (#420)

--- formulaire/pages/api/sync/inc/docs/index.tsx
+++ formulaire/pages/api/sync/inc/docs/index.tsx
@@ -1,5 +1,6 @@
 import { withSentry } from "@sentry/nextjs";
 import type { NextApiHandler } from "next";
+import prisma from "../../../../../src/lib/prismaClient";
 
 const handler: NextApiHandler = async (req, res) => {
     if (req.method == "GET") {
@@ -11,9 +12,64 @@ const handler: NextApiHandler = async (req, res) => {
 };
 
 const get: NextApiHandler = async (req, res) => {
-    const data = JSON.parse(req.body)
-    console.log('data to treat for docs : ', data)
-    res.status(200).json({ message: "OK" });
+    const externalId = req.query.externalId;
+    const token = req.query.token;
+
+    var jwt = require('jsonwebtoken');
+
+    if(token !== process.env.API_KEY_SDP) {
+        res.status(500).json({message: 'Unauthorized'})
+    } else {
+        const dossier = await prisma.dossier.findUnique({
+            where: {
+                id: parseInt(externalId as string)
+            },
+            include: {
+                piecesDossier: true,
+                enfants: {
+                    include: {
+                        piecesDossier: true
+                    }
+                }
+            }
+        })
+        res.status(200).json({
+            dossier: { 
+                id: dossier?.id, 
+                piecesDossier: dossier?.piecesDossier.map((piece) => {
+                    let payload = {
+                        iat: new Date().getTime() / 1000,
+                        id: piece.id,
+                        dossierId: dossier.id,
+                        path: piece.link,
+                    }
+                    let tokenSDP = jwt.sign({...payload}, process.env.SECRET_KEY_DOCS, { expiresIn: 60 * 30 });
+                    return {
+                        type: piece.type,
+                        link: `${process.env.NEXTAUTH_URL}/docs?token=${tokenSDP}`
+                    }
+                })
+            }, 
+            enfants: dossier?.enfants.map((enfant) => {
+                return {
+                    id: enfant.id, 
+                    piecesDossier: enfant.piecesDossier.map((piece) => {
+                        let payload = {
+                            iat: new Date().getTime() / 1000,
+                            id: piece.id,
+                            dossierId: dossier.id,
+                            path: piece.link,
+                        }
+                        let tokenSDP = jwt.sign({...payload}, process.env.SECRET_KEY_DOCS, { expiresIn: 60 * 30 });
+                        return {
+                            type: piece.type,
+                            link: `${process.env.NEXTAUTH_URL}/docs?token=${tokenSDP}`
+                        }
+                    })
+                }
+            })
+        });
+    }
 };
 
 export default withSentry(handler);
