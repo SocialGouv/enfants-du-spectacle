fd580adefb25e66dafc2430eb24ed8b5633b9f27
FredericNumericite <frederic@numericite.eu>
2023-01-31 15:50:45 +0100

    feat: add stats and accessibility in footer

--- formulaire/src/components/Footer.tsx
+++ formulaire/src/components/Footer.tsx
@@ -1,29 +1,31 @@
 import {
-    Footer as FooterDSE,
-    FooterBody,
-    FooterBottom,
-    FooterLink,
-    FooterPartners,
-    FooterPartnersLogo,
-    FooterPartnersTitle,
-    Logo,
-  } from "@dataesr/react-dsfr";
-  import type { ReactElement } from "react";
-  import React from "react";
-  import styles from "../components/ConnexionForm.module.scss";
-  
-  export default function Footer(): ReactElement {
-    return (
-      <FooterDSE className={styles.footer}>
-        <FooterBody description="Simplifier la démarche de demande d’autorisation d’emplois d’enfants du spectacle tout en garantissant une plus grande protection des mineurs de moins de 16 ans travaillant dans ce secteur">
-          <Logo>Préfet de la région Île-de-France</Logo>
-          {/* <FooterBodyItem>
+  Footer as FooterDSE,
+  FooterBody,
+  FooterBottom,
+  FooterLink,
+  FooterPartners,
+  FooterPartnersLogo,
+  FooterPartnersTitle,
+  Logo,
+} from "@dataesr/react-dsfr";
+import type { ReactElement } from "react";
+import React from "react";
+import styles from "../components/ConnexionForm.module.scss";
+
+export default function Footer(): ReactElement {
+  return (
+    <FooterDSE className={styles.footer}>
+      <FooterBody description="Simplifier la démarche de demande d’autorisation d’emplois d’enfants du spectacle tout en garantissant une plus grande protection des mineurs de moins de 16 ans travaillant dans ce secteur">
+        <Logo>Préfet de la région Île-de-France</Logo>
+        {/* <FooterBodyItem>
             <a href="https://next-auth.js.org">Documentation</a>
           </FooterBodyItem> */}
-        </FooterBody>
-        <FooterBottom>
-          <FooterLink href="/mentions">Mentions légales</FooterLink>
-        </FooterBottom>
-      </FooterDSE>
-    );
-  }
+      </FooterBody>
+      <FooterBottom>
+        <FooterLink href="/mentions">Mentions légales</FooterLink>
+        <FooterLink href="/stats">Statistique</FooterLink>
+        <FooterLink href="#">Accessibilité : non conforme</FooterLink>
+      </FooterBottom>
+    </FooterDSE>
+  );
+}
