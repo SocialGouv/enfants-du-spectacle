1bb31e0bd8f995cb1aee03ba18a497d8bc77fbcf
YoannNumericite <85347776+YoannNumericite@users.noreply.github.com>
2023-05-23 08:17:50 +0200

    fix: test upload docs medecine (#549)

--- formulaire/next.config.js
+++ formulaire/next.config.js
@@ -5,8 +5,9 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/api/(.*)",
+        source: "/api/:path*",
         headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "https://enfants-du-spectacle-preprod.dev.fabrique.social.gouv.fr" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
