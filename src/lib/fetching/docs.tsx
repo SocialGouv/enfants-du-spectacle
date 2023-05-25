fa8a9c6357ef18bccfb2d20b93358cd802b3dfcf
Madyo <y.lebars@numericite.eu>
2023-05-25 18:40:25 +0200

    feat: parcours medecine doc correctly uploaded

--- src/lib/fetching/docs.tsx
+++ src/lib/fetching/docs.tsx
@@ -2,7 +2,7 @@ import { JustificatifEnfant } from "@prisma/client";
 
 const uploadDoc = async (data: FormData, dossierId: string, enfantId: string, typeJustif?: JustificatifEnfant): Promise<{filePath: string}> => {
     //const url = `/api/sync/out/docs?dossierId=${dossierId}&typeJustif=${typeJustif}&enfantId=${enfantId}`;
-    const url = `https://formulaire-enfants-du-spectacle-preprod.dev.fabrique.social.gouv.fr/api/sync/inc/upload?id=${dossierId}&typeJustif=${typeJustif}&enfantId=${enfantId}`
+    const url = `${process.env.NEXT_PUBLIC_API_URL_SDP}/inc/upload?id=${dossierId}&typeJustif=${typeJustif}&enfantId=${enfantId}`
     const fetching = await fetch(url, {
         body: data,
         method: "POST"
