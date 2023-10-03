a6f7386e38e1f7abe67296b15a51f1f9825b8622
FredericNumericite <frederic@numericite.eu>
2023-10-03 16:47:31 +0200

    feat: show user email on comment section

--- formulaire/src/components/ListComments.tsx
+++ formulaire/src/components/ListComments.tsx
@@ -1,51 +1,63 @@
 import { Comments } from "@prisma/client";
 import React from "react";
 import styles from "./ListComments.module.scss";
-import useStateContext from "src/context/StateContext";
+import { useSession } from "next-auth/react";
 import { frenchDateHour } from "src/lib/helpers";
 
 interface Props {
-    comments: Comments[]
+  comments: Comments[];
 }
 
-const ListComments: React.FC<Props> = ({ comments, title }) => {
-
-    return (
-        <>
-            <div className={styles.listComments}>
-
-                {comments.length === 0 ?
-                <span>Aucun commentaire.</span>
-                :
-                comments.sort((a, b) => {
-                    if (a.date < b.date) {
-                      return 1;
-                    }
-                    if (a.date > b.date) {
-                      return -1;
-                    }
-                    return 0;
-                  }).map((comment) => (
-                    <CardComment comment={comment} key={comment.id}></CardComment>
-                ))}
-            </div>
-        </>
-    );
+const ListComments: React.FC<Props> = ({ comments }) => {
+  return (
+    <>
+      <div className={styles.listComments}>
+        {comments.length === 0 ? (
+          <span>Aucun commentaire.</span>
+        ) : (
+          comments
+            .sort((a, b) => {
+              if (a.date < b.date) {
+                return 1;
+              }
+              if (a.date > b.date) {
+                return -1;
+              }
+              return 0;
+            })
+            .map((comment) => (
+              <CardComment comment={comment} key={comment.id}></CardComment>
+            ))
+        )}
+      </div>
+    </>
+  );
 };
 
 interface CommentsProps {
-    comment: Comments
+  comment: Comments;
 }
 
 const CardComment: React.FC<CommentsProps> = ({ comment }) => {
-    return (
-        <div className={`${styles.cardComment} ${comment.source === 'INSTRUCTEUR' ? styles.receivedComment : ''}`}>
-            <div className={styles.headerComment}>
-                <b>Commentaire</b> - {frenchDateHour(comment.date ?? new Date())}
-            </div>
-            <p className={styles.textComment} dangerouslySetInnerHTML={{ __html: comment.text.replace(/\n/g,"<br />") }}></p>
-        </div>
-    )
-}
+  const { data: session } = useSession();
+  return (
+    <div
+      className={`${styles.cardComment} ${
+        comment.source === "INSTRUCTEUR" ? styles.receivedComment : ""
+      }`}
+    >
+      <div className={styles.headerComment}>
+        <b>{session.dbUser.email}</b> -{" "}
+        {frenchDateHour(comment.date ?? new Date())}
+      </div>
+      <p
+        className={styles.textComment}
+        dangerouslySetInnerHTML={{
+          __html: comment.text.replace(/\n/g, "<br />"),
+        }}
+      ></p>
+    </div>
+  );
+};
 
 export default ListComments;
