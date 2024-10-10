2299c08290a1bccdc6984fc7c6cc11c3a2c5d34d
Matéo Mévollon <m.mevollon@proton.me>
2024-10-10 18:36:33 +0200

    reset prisma pool

--- src/lib/prismaClient.ts
+++ src/lib/prismaClient.ts
@@ -9,7 +9,7 @@ export const client =
   new PrismaClient({
     datasources: {
       db: {
-        url: process.env.DATABASE_URL + "&connection_limit=50",
+        url: process.env.DATABASE_URL + "&connection_limit=5",
       },
     },
     log: ["info", "warn", "error"],
