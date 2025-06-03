cc0ea21543a49d5dc1cf5d8bcf91170929e7e279
YoannNumericite <y.lebars@numericite.eu>
2025-06-03 11:03:36 +0200

    feat: colors status

--- formulaire/src/components/LabelStatus.tsx
+++ formulaire/src/components/LabelStatus.tsx
@@ -2,6 +2,7 @@
 import { StatutDossier } from "@prisma/client";
 import styles from "./LabelStatus.module.scss";
 import React from "react";
+import { factory as statutDossierStateMachineFactory } from "../lib/statutDossierStateMachine";
 
 interface Props {
     status: StatutDossier;
@@ -9,49 +10,14 @@ interface Props {
 }
 
 const LabelStatus: React.FC<Props> = ({ status, wihteBackground }) => {
-
-
-    switch(status) {
-        case 'BROUILLON':
-            return (
-                <div className={`${styles.labelStatus} ${styles.blue} ${wihteBackground ? `${styles.white}` : ''}`}>
-                    {status}
-                </div>
-            );
-        case 'CONSTRUCTION':
-        case 'INSTRUCTION':
-            return (
-                <div className={`${styles.labelStatus} ${styles.yellow} ${wihteBackground ? `${styles.white}` : ''}`}>
-                    {`EN ${status}`}
-                </div>
-            );
-        case 'PRET':
-        case 'AVIS_FAVORABLE': 
-        case 'AVIS_FAVORABLE_SOUS_RESERVE':
-            return (
-                <div className={`${styles.labelStatus} ${styles.green} ${wihteBackground ? `${styles.white}` : ''}`}>
-                    {status.replaceAll('_', ' ')}
-                </div>
-            );
-        case 'ACCEPTE':
-            return (
-                <div className={`${styles.labelStatus} ${styles.accepted} ${wihteBackground ? `${styles.white}` : ''}`}>
-                    {status}
-                </div>
-            );
-        case 'REFUSE':
-            return (
-                <div className={`${styles.labelStatus} ${styles.refused} ${wihteBackground ? `${styles.white}` : ''}`}>
-                    {status}
-                </div>
-            );
-        default: 
-            return (
-                <div className={`${styles.labelStatus} ${styles.default} ${wihteBackground ? `${styles.white}` : ''}`}>
-                    {status.replaceAll('_', ' ')}
-                </div>
-            );
-    }
+    const stateMachine = statutDossierStateMachineFactory(status);
+    const stateClassName = stateMachine.stateClassName();
+    
+    return (
+        <div className={`${styles.labelStatus} ${stateClassName} ${wihteBackground ? `${styles.white}` : ''}`}>
+            {status === 'CONSTRUCTION' || status === 'INSTRUCTION' ? `EN ${status}` : status.replaceAll('_', ' ')}
+        </div>
+    );
 };
 
 export default LabelStatus;
